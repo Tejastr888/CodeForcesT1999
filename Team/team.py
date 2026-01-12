9e09c75ce49953665481b11e68bf62c3e3a94e13
Tejas R <tejasrt45@gmail.com>
2026-01-12 22:51:39 +0530

    correction in Team problem

--- Team/team.py
+++ Team/team.py
@@ -3,7 +3,7 @@ arr=[[int(x) for x in input().strip().split()] for _ in range(n)]
 ans=0
 for i in range(n):
     curr=0
-    for j in range(n):
+    for j in range(len(arr[i])):
         if arr[i][j]==1:
             curr+=1
     if curr>=2:
